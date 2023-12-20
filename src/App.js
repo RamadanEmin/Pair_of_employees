9673935b12e05111db6d332673493688602a85d0
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2023-12-20 23:41:46 +0200

    Added Error context

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Employee from './components/Employee/Employee';
-import { DataProvider } from './context/data-context';
+import { DataProvider } from './context/dataContext';
+import { ErrorProvider } from './context/errorContext';
 
 function App() {
     return (
-        <DataProvider>
-            <Employee />
-        </DataProvider>
+        <ErrorProvider>
+            <DataProvider>
+                <Employee />
+            </DataProvider>
+        </ErrorProvider>
     );
 }
 
---
+++ src/context/errorContext.js
@@ -0,0 +1,23 @@
+import {createContext, useContext, useState} from 'react';
+
+const ErrorContext = createContext();
+
+const ErrorProvider = ({children}) => {
+    const [error, setError] = useState(null);
+
+    return (
+        <ErrorContext.Provider value={{error, setError}}>
+            {children}
+        </ErrorContext.Provider>
+    );
+};
+
+const useError = () => {
+    const context = useContext(ErrorContext);
+    if (context === undefined) {
+        throw new Error('useError must be used within a ErrorProvider');
+    }
+    return context;
+};
+
+export {ErrorProvider, useError};
